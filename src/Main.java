825f2847f92a8dd75691e2c08ee09adf4978dd8b
”Buharin <black_cloak56@mail.ru>
2021-03-01 23:48:24 +0500

    MAX MIN VALUE

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("byte max: " + Byte.MAX_VALUE + " byte min: " + Byte.MIN_VALUE);
+        System.out.println("int max: " + Integer.MAX_VALUE + " int min: " + Integer.MIN_VALUE);
+        System.out.println("short max: " + Short.MAX_VALUE + " short min: " + Short.MIN_VALUE);
+        System.out.println("long max: " + Long.MAX_VALUE + " long min: " + Long.MIN_VALUE);
+        System.out.println("float max: " + Float.MAX_VALUE + " float min: " + Float.MIN_VALUE);
+        System.out.println("double max: " + Double.MAX_VALUE + " double min: " + Double.MIN_VALUE);
+
+    }
+}
